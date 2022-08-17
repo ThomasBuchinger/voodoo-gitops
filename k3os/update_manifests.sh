40e38c1e366c7f26ba4d4b0a6ba01f8521892b05
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-08-18 00:28:52 +0200

    Fix HELM Chart url and update_manifests script

--- k3os/update_manifests.sh
+++ k3os/update_manifests.sh
@@ -1,10 +1,17 @@
-RRANCH=${1:-main}
+BRANCH=${1:-main}
 tmp=$(mktemp -d)
 
 
+echo "Download Archive..."
 cd $tmp
-curl -o voodoo-gitops.zip -L https://github.com/ThomasBuchinger/voodoo-gitops/archive/refs/heads/${BRANCH}.zip
-unzip voodoo-gitops.zip
+status_code=$(curl --write-out '%{http_code}' -O -L https://github.com/ThomasBuchinger/voodoo-gitops/archive/refs/heads/${BRANCH}.zip)
+if [ "200" != "$status_code" ]; then
+  echo "Unable to download ZIP for '${BRANCH}'. HTTP Status: ${status_code}"
+  exit 1
+fi
 
+unzip "${BRANCH}.zip" > /dev/null
+
+echo "Copying Manifests..."
 mkdir -p /var/lib/rancher/k3s/server/manifests/gitops/
-rsync -a --delete gitops/voodoo-gitops-${BRANCH}/gitops /var/lib/rancher/k3s/server/manifests/
+rsync -avh --delete "voodoo-gitops-${BRANCH}/gitops" "/var/lib/rancher/k3s/server/manifests/"
