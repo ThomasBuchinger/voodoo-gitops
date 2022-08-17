f901a8e8cf56345749960bef6445abe6e54392a0
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-08-18 00:06:57 +0200

    Move PiHole to default namespace

--- k3os/update_manifests.sh
+++ k3os/update_manifests.sh
@@ -1,7 +1,10 @@
+RRANCH=${1:-main}
 tmp=$(mktemp -d)
-cd $tmp
-curl -o voodoo-gitops.zip -L https://github.com/ThomasBuchinger/voodoo-gitops/archive/refs/heads/main.zip
-unzip voodoo-gitops.zip -d gitops
 
-#mkdir -p /var/lib/rancher/k3s/server/manifests/gitops/
-rsync -a --delete gitops /var/lib/rancher/k3s/server/manifests/
+
+cd $tmp
+curl -o voodoo-gitops.zip -L https://github.com/ThomasBuchinger/voodoo-gitops/archive/refs/heads/${BRANCH}.zip
+unzip voodoo-gitops.zip
+
+mkdir -p /var/lib/rancher/k3s/server/manifests/gitops/
+rsync -a --delete gitops/voodoo-gitops-${BRANCH}/gitops /var/lib/rancher/k3s/server/manifests/
