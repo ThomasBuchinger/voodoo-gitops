a952138719f67187c1e2ee70037d00a2ccb375e6
Thomas Buchinger <ThomasBuchinger@users.noreply.github.com>
2022-09-03 20:16:37 +0200

    Add OKD Console (#3)
    Add OKD Console in ReadOnly Mode as a simple WebUI

--- k3os/cron_update_images.sh
+++ k3os/cron_update_images.sh
@@ -2,3 +2,4 @@ k3s crictl pull pihole/pihole:latest
 k3s crictl pull quxfoo/wastebin:latest
 k3s crictl pull cloudflare/cloudflared:latest
 k3s crictl pull redhat/ubi9:latest
+k3s crictl pull quay.io/openshift/origin-console:latest
