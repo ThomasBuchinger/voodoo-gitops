2ae1b58d7ab3f4f1f8ade4eb50b4355089b8ed45
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-09-03 18:26:55 +0200

    Change base image to debian, because we need bash

--- k3os/cron_update_images.sh
+++ k3os/cron_update_images.sh
@@ -1,4 +1,5 @@
 k3s crictl pull pihole/pihole:latest
 k3s crictl pull quxfoo/wastebin:latest
 k3s crictl pull cloudflare/cloudflared:latest
+k3s crictl pull debian:latest
 k3s crictl pull alpine:latest
