ae52fe43ab46511b1f0c9d10099c88c7d9e3654d
Thomas Buchinger <ThomasBuchinger@users.noreply.github.com>
2022-09-11 03:15:14 +0200

    Implement External-Secrets and replace some SealedSecrets (#7)
    * Configure Vault to for K8s-auth
    * Add Secrets on Vault start up
    * Consolidate Secrets
    * Configure ExternalSecrets to use Vault
    * Replace SealedSecrets in cloudflare
    * Replace SealedSecrets in DDNS Shell
    * Move pihole to common-secret

--- k3os/install_flux.sh
+++ k3os/install_flux.sh
@@ -5,3 +5,4 @@ BRANCH=${1:-main}
 curl -s https://fluxcd.io/install.sh | sudo bash
 curl -L -o $MANIFEST_DIR/flux-system.yaml https://raw.githubusercontent.com/ThomasBuchinger/voodoo-gitops/$BRANCH/flux-install/flux-system.yaml
 curl -L -o $MANIFEST_DIR/voodoo-gitops.yaml https://raw.githubusercontent.com/ThomasBuchinger/voodoo-gitops/$BRANCH/flux-install/voodoo-gitops.yaml
+sed -i -e "s/branch: main/branch: $BRANCH/"  $MANIFEST_DIR/voodoo-gitops.yaml
