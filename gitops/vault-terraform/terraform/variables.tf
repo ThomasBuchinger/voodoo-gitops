43b48568bd229f030772270473eaded65be6210f
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-09-16 23:14:27 +0200

    Improve variable handling

--- gitops/vault-terraform/terraform/variables.tf
+++ gitops/vault-terraform/terraform/variables.tf
@@ -1,11 +1,11 @@
-variable "vault-address" {
-  type        = string
-  sensitive   = false
-  default     = "http://vault:8200"
-  description = "Vault Root Token"
-}
-variable "vault-root" {
-  type        = string
-  sensitive   = true
-  description = "Vault Root Token"
-}
+# variable "vault-address" {
+#   type        = string
+#   sensitive   = false
+#   default     = "http://vault:8200"
+#   description = "Vault Root Token"
+# }
+# variable "vault-root" {
+#   type        = string
+#   sensitive   = true
+#   description = "Vault Root Token"
+# }
