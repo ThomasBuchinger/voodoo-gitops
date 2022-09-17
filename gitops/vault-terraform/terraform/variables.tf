a59e4008be7c95b6278f5fdf2aa7e93a31dbad1f
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-09-17 03:33:12 +0200

    set admin user password

--- gitops/vault-terraform/terraform/variables.tf
+++ gitops/vault-terraform/terraform/variables.tf
@@ -10,3 +10,18 @@ variable "vault_role" {
   default     = "terraform"
   description = "Vault Role to authenticate with"
 }
+
+variable "admin_user" {
+  type        = string
+  sensitive   = false
+  default     = "admin"
+  description = "username for the default admin user"
+}
+
+variable "admin_passwd" {
+  type        = string
+  sensitive   = true
+  description = "password for the default admin"
+}
+
+
