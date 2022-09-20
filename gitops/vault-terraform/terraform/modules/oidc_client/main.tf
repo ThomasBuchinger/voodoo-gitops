ebc5c97f3fcead9db7187529a2599fd676083260
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-09-21 01:19:57 +0200

    Configure OIDC Provider in Vault

---
+++ gitops/vault-terraform/terraform/modules/oidc_client/main.tf
@@ -0,0 +1,51 @@
+
+
+resource "vault_identity_oidc_key" "key" {
+  name               = var.client_name
+  allowed_client_ids = ["*"]
+  rotation_period    = 3600
+  verification_ttl   = 3600
+}
+
+
+resource "vault_identity_oidc_assignment" "authorized_groups" {
+  name       = var.client_name
+  entity_ids = var.authorized_users
+  group_ids  = var.authorized_users
+}
+resource "vault_identity_oidc_client" "client_config" {
+  name          = var.client_name
+  key           = vault_identity_oidc_key.key.name
+  redirect_uris = [
+    "http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback",
+    "http://127.0.0.1:8251/callback",
+    "http://127.0.0.1:8080/callback"
+  ]
+  assignments = [
+    "allow_all"
+  ]
+  id_token_ttl     = 2400
+  access_token_ttl = 7200
+}
+
+variable "client_name" {
+  type = string
+}
+
+variable "authorized_users" {
+  type = list(string)
+  default = [ ]
+}
+variable "authorized_groups" {
+  type = list(string)
+  default = [ ]
+}
+
+output "client_id" {
+  value = vault_identity_oidc_client.client_config.client_id
+}
+
+output "client_secret" {
+  value = vault_identity_oidc_client.client_config.client_secret
+  sensitive = true
+}
--- gitops/vault-terraform/terraform/oidc.tf
+++ gitops/vault-terraform/terraform/oidc.tf
@@ -1,5 +1,16 @@
+
+resource "vault_identity_oidc_provider" "vault_oidc_provider" {
+  name = "vault_oidc"
+  https_enabled = false
+  issuer_host = "127.0.0.1:8200"
+  allowed_client_ids = [
+    module.oidc_kubernetes.client_id
+  ]
+}
+
+
 module "oidc_kubernetes" {
   source = "./modules/oidc_client"
 
-  
+  client_name = "kubelogin"
 }
