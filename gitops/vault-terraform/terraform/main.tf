ed946a3a6499ee31e5663569a1a7ac628f2cce18
Thomas Buchinger <thomas.buchinger@outlook.com>
2022-09-24 16:23:06 +0200

    Add approle auth to terraform

--- gitops/vault-terraform/terraform/main.tf
+++ gitops/vault-terraform/terraform/main.tf
@@ -7,6 +7,10 @@ resource "vault_auth_backend" "static" {
     listing_visibility = "unauth"
   }
 }
+data "vault_auth_backend" "approle" {
+  type = "approle"
+  description = "Auth Backend for ServiceAccounts outside of Kubernetes"
+}
 
 module "kubeconfig" {
   source = "./modules/kubeconfig"
@@ -17,4 +21,13 @@ module "kubeconfig" {
   client_id = module.oidc_kubernetes.client_id
   client_secret = module.oidc_kubernetes.client_secret
   oidc_provider_url = module.oidc_kubernetes.oidc_provider_url
+}
+
+module "approle_green" {
+  source = "./modules/approle"
+  
+  name = "green"
+  secretid_fetch_mount = "secret"
+  secretid_fetch_path = "access/secretids"
+  secretid_fetch_key = "green"
 }
---
+++ gitops/vault-terraform/terraform/modules/approle/main.tf
@@ -0,0 +1,61 @@
+locals {
+  role_name = "external-kubernetes-${var.name}"
+  role_id = var.name
+}
+
+data "vault_kv_secret_v2" "secret_id" {
+  mount = var.secretid_fetch_mount
+  name = var.secretid_fetch_path
+}
+
+resource "vault_approle_auth_backend_role" "role" {
+  backend         = var.approle_path
+  role_name       = local.role_id
+  role_id         = local.role_name
+  token_policies  = ["default", "cluster_reader"]
+  token_ttl = 300
+  token_max_ttl = 300
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "secret_id" {
+  backend   = var.approle_path
+  role_name = vault_approle_auth_backend_role.role.role_name
+  secret_id = data.vault_kv_secret_v2.secret_id.data[var.secretid_fetch_key]
+  metadata = jsonencode({
+    role_name = local.role_name
+    role_id = local.role_id
+    secretid_secret_mount = var.secretid_fetch_mount
+    secretid_secret = "${var.secretid_fetch_mount}/${var.secretid_fetch_path}"
+  })
+}
+
+resource "vault_approle_auth_backend_login" "login" {
+  backend   = var.approle_path
+  role_id   = vault_approle_auth_backend_role.role.role_id
+  secret_id = vault_approle_auth_backend_role_secret_id.secret_id.secret_id
+}
+
+
+variable "name" {
+  type = string
+}
+
+variable "secretid_fetch_mount" {
+  type = string
+}
+
+variable "secretid_fetch_path" {
+  type = string
+}
+variable "secretid_fetch_key" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "approle_path" {
+  type = string
+  default = "approle/"
+}
