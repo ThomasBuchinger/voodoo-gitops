413b824a8b9832ecb5d981e615754e22ef303005
Thomas Buchinger <thomas.buchinger@outlook.com>
2023-01-06 14:34:01 +0100

    Reotate SealedSecrets encryption

--- install_k3s.sh
+++ install_k3s.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-function open_firewall() {
-  local port=$1
-  if ! firewall-cmd --quiet --query-port ${port}; then
-    echo "Open Firewall Port: ${port}"
-    firewall-cmd --permanent --add-port ${port}
-    firewall-cmd --reload
-  fi
-}
 
 # Some Base OS config
 #
 dnf install -y vim
-dnf install -y cockpit cockpit-navigator cockpit-networkmanager cockpit-packagekit cockpit-pcp cockpit-selinux cockpit-sosreport
+dnf install -y cockpit cockpit-navigator cockpit-networkmanager cockpit-packagekit cockpit-pcp cockpit-selinux cockpit-sosreport tar
 systemctl enable --now cockpit.socket
-open_firewall 9090/tcp
 
 
 # Install k3s
 #
+systemctl disable --now firewalld
 K3S_CONFIG="/etc/rancher/k3s/config.yaml"
 mkdir -p $(dirname $K3S_CONFIG)
 echo "selinux: true" > $K3S_CONFIG
 curl -sfL https://get.k3s.io | sh -s -
-open_firewall 6443/tcp
-
+mkdir -p ~/.kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 
 # Isntall Fluc
 #
