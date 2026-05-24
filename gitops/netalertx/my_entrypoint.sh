c0d58d4021ffa527eb8f3dc8bd77d73a80d30fa6
Thomas Buchinger <thomas.buchinger@outlook.com>
2026-05-24 22:03:19 +0200

    fix(netalertx): add dedicated import script

--- gitops/netalertx/my_entrypoint.sh
+++ gitops/netalertx/my_entrypoint.sh
@@ -4,6 +4,6 @@ mkdir -p /data/{config,db}
 chmod -R +rw /data/{config,db}
 cp /config/app.conf /data/config/
 echo -e "\nMQTT_PASSWORD='${MQTT_PASSWORD}'" >> /data/config/app.conf
-echo -e "\API_TOKEN='${API_TOKEN}'" >> /data/config/app.conf
+echo -e "\nAPI_TOKEN='${API_TOKEN}'" >> /data/config/app.conf
 
 exec /entrypoint.sh
