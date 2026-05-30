cc533685ddeefb11e589b09755502659170c889f
Thomas Buchinger <thomas.buchinger@outlook.com>
2026-05-30 20:36:39 +0200

    enable webui

--- k3s/install_flux.sh
+++ k3s/install_flux.sh
@@ -5,4 +5,4 @@ BRANCH=${1:-main}
 curl -s https://fluxcd.io/install.sh | sudo bash
 curl -L -o $MANIFEST_DIR/flux-operator.yaml https://raw.githubusercontent.com/ThomasBuchinger/voodoo-gitops/$BRANCH/flux-install/flux-operator.yaml
 curl -L -o $MANIFEST_DIR/voodoo-gitops.yaml https://raw.githubusercontent.com/ThomasBuchinger/voodoo-gitops/$BRANCH/flux-install/voodoo-gitops.yaml
-sed -i -e "s|refs/heads/main|/refs/heads/$BRANCH|"  $MANIFEST_DIR/voodoo-gitops.yaml
+sed -i -e "s|refs/heads/main|refs/heads/$BRANCH|"  $MANIFEST_DIR/voodoo-gitops.yaml
